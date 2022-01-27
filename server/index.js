11fc4d46fc2b2a3661685969c7e00fbbb7f00838
erik savage <erik.k.savage@gmail.com>
2022-01-26 20:17:59 -0800

    adds queue to cache shipments and logs

--- server/index.js
+++ server/index.js
@@ -1,10 +1,42 @@
 'use strict';
 
+const uuid = require('uuid').v4;
 const socketio = require('socket.io');
+
 const PORT = process.env.PORT || 3000;
 const server = socketio(PORT);
-
 const caps = server.of('/caps');
+
+const queue = {
+  logs: {},
+  shipments: {},
+  addLog: function (log) {
+    //function to add a log
+    let id = uuid();
+    this.logs[id] = log;
+    return {
+      id,
+      payload: log,
+    };
+  },
+  removeLog: function (id) {
+    //function to remove a log
+    delete queue.logs[id];
+  },
+  addShipment: function (shipment) {
+    //function to add a shipment to the shipmnents log
+    let id = uuid();
+    this.shipments[id] = shipment;
+    return {
+      id,
+      payload: shipment,
+    };
+  },
+  removeShipment: function (id) {
+    //function to remove a shipment
+    delete queue.shipments[id];
+  },
+};
 
 class EVENT {
   constructor(event, time, payload) {
